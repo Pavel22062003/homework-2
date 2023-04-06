3b85d6f443f3b8e5c7d27c1239e05442ceb8ece7
Pavel22062003 <123014373+Pavel22062003@users.noreply.github.com>
2023-04-06 23:07:28 +0200

    Create filter_sorting.sql

---
+++ filter_sorting.sql
@@ -0,0 +1,17 @@
+--1
+SELECT * FROM orders
+WHERE ship_country in ('France', 'Germany', 'Spain')
+--2
+SELECT DISTINCT ship_country, ship_city FROM orders
+ORDER BY ship_country, ship_city
+--3
+SELECT shipped_date - order_date as total_ship_days FROM orders
+WHERE ship_country = 'Germany'
+--4
+SELECT MIN(unit_price), MAX(unit_price) FROM products
+WHERE discontinued <> 1
+--5
+
+SELECT MIN(unit_price), MAX(unit_price) FROM products
+WHERE discontinued <> 1 AND units_in_stock >= 20
+
